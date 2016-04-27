04a20d62818f5433fac31443e124422f34a09874
crapthings <crapthings@gmail.com>
2016-04-27 21:47:57 +0800

    fucking ill

--- index.js
+++ index.js
@@ -2,12 +2,50 @@ import express from 'express'
 
 import { apolloServer } from 'graphql-tools'
 
+import _ from 'lodash'
+
+import faker from 'faker'
+
 const server = express()
+
+//
+
+class user {
+	constructor (opt) {
+		this.username = faker.name.findName()
+		this.age = faker.random.number()
+		this.desc = this.username + this.age
+		opt && _.extend(this, opt)
+	}
+}
+
+class post {
+	constructor (opt) {
+		this.title = faker.lorem.sentence()
+	}
+}
+
+//
 
 const schema = `
 
+type User {
+	username: String # 这个可以啪啪啪
+	age: Int
+	desc: String
+	type: String
+	posts: [Post]
+}
+
+type Post {
+	title: String
+	user: User
+}
+
 type RootQuery {
-	lala: String
+	user: User
+	users: [User]
+	posts: [Post]
 }
 
 schema {
@@ -16,10 +54,35 @@ schema {
 
 `
 
+//
+
 const resolvers = {
 	RootQuery: {
-		lala () {
-			return 'haha'
+		users () {
+			return _.times(10, n => {
+				return new user()
+			})
+		},
+		user () {
+			return new user()
+		},
+
+		posts () {
+			return _.times(10, n => {
+				return new post()
+			})
+		}
+	},
+	User: {
+		posts () {
+			return _.times(10, n => {
+				return new post()
+			})
+		}
+	},
+	Post: {
+		user () {
+			return new user()
 		}
 	}
 }
